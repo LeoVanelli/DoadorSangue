2aaff37edbe56e74c5ff26a477808086fdbde3be
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 19:55:32 -0300

    Adiciona atividade DoadorSangue

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Avaliação para Doador de Sangue --");
+            Console.Write("Usuário, qual sua idade ? ");
+            int _idade = int.Parse(Console.ReadLine());
+
+            if(_idade > 67 || _idade < 18){
+                Console.WriteLine("Você não pode ser doador de sangue.");
+            }
+            else{
+                Console.WriteLine("Você pode ser doador de sangue.");
+            }
+        }
+    }
+}
